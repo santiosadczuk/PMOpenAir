58488551c3897c1ba75dcc53ae2c75c93c7aa66b
santiosadczuk <sosadczuk@hotmail.com>
2019-05-03 11:24:28 -0300

    styile

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -8,45 +8,16 @@ import { map } from 'rxjs/operators';
 })
 export class SpotifyService {
 
-  token: any;
-  body = new URLSearchParams({
-    grant_type: "client_credentials"
-  });
-  header = new HttpHeaders({
-    Authorization:
-      "Basic <base64 encoded  050b1e19c2ba427cb0af4e20252b680e:b34482186150439d9d074da60d5ed545>"
-  });
-  constructor(private http: HttpClient) {
-    this.token =
-      "BQD-GxNbLXPNQZEnLLZJujcqhArgA4sj3bLr1OJDMsfpJzdFkUmlDpE63yx5o5kDXxsqs2ZAR1QHufYzXSg";
-  }
-  getToken() {
-    this.http
-      .post("https://accounts.spotify.com/api/token", this.body, {
-        headers: this.header
-      })
-      .subscribe(data => {
-
-        return data;
-      });
-  }
+  constructor(private http: HttpClient) {}
+  
   getQuery(query: string) {
     const url = `https://api.spotify.com/v1/${query}`;
     const headers = new HttpHeaders({
-      Authorization: "Bearer " + this.token
+      Authorization: "Bearer BQATbiEI6yrlrzlDYxz0m2jyVj9Wk6ZvmdJG698mFiqRVxqHLk_3-6WxBoRTgCnyI2wwzW5kjIn4yuDmrhw"
     });
     return this.http.get(url, { headers });
   }
-
-  /*
-  getQuery(query: string) {
-    const url = `https://api.spotify.com/v1/${query}`;
-    const headers = new HttpHeaders({
-      Authorization: "Bearer BQBBGh_VE6vBa6cc3tlX6LZZmLpKwwrCc4esMBtq8TGFFukHK94vf8007SZxtOTm8i0g1-PqhrxiU43D7m4"
-    });
-    return this.http.get(url, { headers });
-  }
-  */
+  
 
   getArtists(termino: string){
 
